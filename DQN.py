c55f0e7f88fd7d42464d2e246051e996af027b83
gold <skgb0909@naver.com>
2018-04-05 15:54:11 +0900

    커밋커밋

---
+++ DQN.py
@@ -0,0 +1,28 @@
+"""
+인하대학교 컴퓨터공확과 컴퓨터 종합설계 프로젝트 5팀
+Tetris 강화학습 실행하는 코드
+
+SourceCode : www.github.com/goldpaper/Capstone5
+"""
+
+import random
+import numpy as np
+import tensorflow as tf
+
+class learnDQN() :
+    def __init__(self):
+        print('test')
+
+    def learnCNN(self):
+        print('test')
+
+    def trainCNN(self):
+
+    def DQN(self):
+        print('test')
+
+    def reward(self):
+        print('test')
+
+    def playTetris(self):
+        print('test')
---
+++ tetris.py
@@ -0,0 +1,6 @@
+"""
+인하대학교 컴퓨터공확과 컴퓨터 종합설계 프로젝트 5팀
+Tetris 프로그램 실행 및 화면 구성 관리 코드
+
+SourceCode : www.github.com/goldpaper/Capstone5
+"""
